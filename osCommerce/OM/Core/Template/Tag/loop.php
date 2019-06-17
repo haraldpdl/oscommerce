0b6b4f559278c297f076b3c533011d96ee49bc6f
Harald Ponce de Leon <hpdl@oscommerce.com>
2019-06-17 22:59:28 +0200

    return an empty string if the value does not exist instead of the key name (same behaviour as value/raw)

--- osCommerce/OM/Core/Template/Tag/loop.php
+++ osCommerce/OM/Core/Template/Tag/loop.php
@@ -2,55 +2,60 @@
 /**
  * osCommerce Online Merchant
  *
- * @copyright Copyright (c) 2014 osCommerce; http://www.oscommerce.com
- * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ * @copyright (c) 2019 osCommerce; https://www.oscommerce.com
+ * @license MIT; https://www.oscommerce.com/license/mit.txt
  */
 
-  namespace osCommerce\OM\Core\Template\Tag;
+namespace osCommerce\OM\Core\Template\Tag;
 
-  use osCommerce\OM\Core\HTML;
-  use osCommerce\OM\Core\OSCOM;
-  use osCommerce\OM\Core\Registry;
+use osCommerce\OM\Core\{
+    HTML,
+    OSCOM,
+    Registry
+};
 
-  class loop extends \osCommerce\OM\Core\Template\TagAbstract {
-    static protected $_parse_result = false;
+class loop extends \osCommerce\OM\Core\Template\TagAbstract
+{
+    protected static $_parse_result = false;
 
-    static public function execute($string) {
-      $args = func_get_args();
+    public static function execute($string)
+    {
+        $args = func_get_args();
 
-      $OSCOM_Template = Registry::get('Template');
+        $OSCOM_Template = Registry::get('Template');
 
-      $key = trim($args[1]);
+        $key = trim($args[1]);
 
-      if ( !$OSCOM_Template->valueExists($key) ) {
-        if ( class_exists('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\Module\\Template\\Value\\' . $key . '\\Controller') && is_subclass_of('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'osCommerce\\OM\\Core\\Template\\ValueAbstract') ) {
-          call_user_func(array('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'initialize'));
-        } elseif ( class_exists('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Module\\Template\\Value\\' . $key . '\\Controller') && is_subclass_of('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'osCommerce\\OM\\Core\\Template\\ValueAbstract') ) {
-          call_user_func(array('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'initialize'));
+        if (!$OSCOM_Template->valueExists($key)) {
+            if (class_exists('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\Module\\Template\\Value\\' . $key . '\\Controller') && is_subclass_of('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'osCommerce\\OM\\Core\\Template\\ValueAbstract')) {
+                call_user_func(['osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Application\\' . OSCOM::getSiteApplication() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'initialize']);
+            } elseif (class_exists('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Module\\Template\\Value\\' . $key . '\\Controller') && is_subclass_of('osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'osCommerce\\OM\\Core\\Template\\ValueAbstract')) {
+                call_user_func(['osCommerce\\OM\\Core\\Site\\' . OSCOM::getSite() . '\\Module\\Template\\Value\\' . $key . '\\Controller', 'initialize']);
+            }
         }
-      }
 
-      $data = $OSCOM_Template->getValue($key);
+        $data = $OSCOM_Template->getValue($key);
 
-      if ( isset($args[2]) ) {
-        $data = $data[$args[2]];
-      }
-
-      $result = '';
-
-      if ( !empty($data) ) {
-        foreach ( $data as $d ) {
-          $result .= preg_replace_callback('/([#|%])([a-zA-Z0-9_-]+)\1/', function ($matches) use (&$d) {
-                       if ( substr($matches[0], 0, 1) == '%' ) {
-                         return ( isset($d[$matches[2]]) ? $d[$matches[2]] : $matches[0] );
-                       } else {
-                         return ( isset($d[$matches[2]]) ? HTML::outputProtected($d[$matches[2]]) : $matches[0] );
-                       }
-                     }, $string);
+        if (isset($args[2])) {
+            $data = $data[$args[2]];
         }
-      }
 
-      return $result;
+        $result = '';
+
+        if (!empty($data)) {
+            foreach ($data as $d) {
+                $result .= preg_replace_callback('/([#|%])([a-zA-Z0-9_-]+)\1/', function ($matches) use (&$d) {
+                    $value = $d[$matches[2]] ?? '';
+
+                    if (substr($matches[0], 0, 1) == '#') {
+                        $value = HTML::outputProtected($value);
+                    }
+
+                    return $value;
+                }, $string);
+            }
+        }
+
+        return $result;
     }
-  }
-?>
+}
